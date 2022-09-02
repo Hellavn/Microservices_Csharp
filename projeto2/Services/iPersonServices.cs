4010c0667e49434037d580940b0ec4f960c219b4
Vandriele Barbosa <43830415+Hellavn@users.noreply.github.com>
2022-09-01 22:02:42 -0300

    Create Startup.cs

--- projeto2/Services/iPersonServices.cs
+++ projeto2/Services/iPersonServices.cs
@@ -1,6 +1,14 @@
-﻿namespace projeto2.Services
+﻿using projeto2.Model;
+
+namespace projeto2.Services
 {
     public interface iPersonServices
     {
+        Person Create(Person person);
+        Person FindByID(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+
     }
 }
--- projeto2/Services/implementations/PersonServiceImplementation.cs
+++ projeto2/Services/implementations/PersonServiceImplementation.cs
@@ -1,6 +1,67 @@
-﻿namespace projeto2.Services.implementations
+﻿using projeto2.Model;
+
+namespace projeto2.Services.implementations
 {
-    public class PersonServiceImplementation
+    public class PersonServiceImplementation : iPersonServices
     {
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+            
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> people = new List<Person>();
+            for (int i = 0; i<8; i++)
+            {
+                Person person = MockPerson(i);
+                people.Add(person);
+            }
+            return people;
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Primeiro Nomes " +i,
+                LastName = "Sobrenome " + i,
+                Address = "Endereço " + i,
+                Gender = "F " 
+
+
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+
+        public Person FindByID(long id)
+        {
+            return new Person
+            {
+                
+                FirstName = "Maria",
+                LastName = "da Dores",
+                Address = "Campos do Jordão - SP",
+                Gender = "F"
+
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
     }
 }
